0ebed715116b4e93604411c2f17120dd6f81b9ab
thanhdc <dang.phongvan.thanh@gmail.com>
2017-02-15 23:30:48 +0700

    Done 6.5

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -6,8 +6,81 @@ var sendResponseJson = function(res,status,content){
     res.json(content);
 }
 
+var theEarth = (function(){
+    var earthRadius = 6371;
+    var getDistanceFromRads = function(rads){
+        return parseFloat(rad * earthRadius);
+    }
+
+    var getRadsFromDistance = function(distance){
+        return parseFloat(distance/earthRadius);
+    }
+
+    return {
+        getDistanceFromRads: getDistanceFromRads,
+        getRadsFromDistance : getRadsFromDistance
+    };
+})();
+
 module.exports.locationsListByDistance = function (req,res) {
-    sendResponseJson(res,200,{"status":"success"});
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var point = {
+        type: "Point",
+        coordinates:[lng,lat]
+    };
+
+    var geoOptions = {
+        spherical:true,
+        maxDistance: theEarth.getRadsFromDistance(20),
+        num:10
+    };
+
+    Loc.geoNear(point,geoOptions,function(err,results,stats){
+        if(err){
+            sendResponseJson(res,404,err);
+        } else {
+            var locations = [];
+            results.forEach(function(doc){
+                location.push({
+                    distance:theEarth.getDistanceFromRads(doc.dis),
+                    name:doc.obj.name,
+                    address:doc.obj.address,
+                    rating: doc.obj.rating,
+                    facilities: doc.obj.facilities,
+                    _id:doc.obj._id
+                });
+            });
+            sendResponseJson(res,200,locations);
+        }
+    });
+}
+//10.738071, 106.627914
+module.exports.locationsCreate = function(req,res){
+    var location = {
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(','),
+        coords:[parseFloat(req.body.lng),parseFloat(req.body.lat)],
+        openingTimes:[{
+            days:req.body.days1,
+            opening:req.body.opening1,
+            closing:req.body.closing1,
+            closed:req.body.closed1,
+        },{
+            days:req.body.days2,
+            opening:req.body.opening2,
+            closing:req.body.closing2,
+            closed:req.body.closed2,
+        }]
+    };
+    Loc.create(location,function(err,location){
+        if(err){
+            sendResponseJson(res,404,err);
+        } else {
+            sendResponseJson(res,201,location);
+        }
+    });
 }
 
 module.exports.locationsReadOne = function(req,res){
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -6,6 +6,78 @@ var sendResponseJson = function(res,status,content){
     res.json(content);
 }
 
+module.exports.reviewsCreate = function(req,res){
+    var locationID = req.params.locationid;
+    if(locationID){
+        Loc.findById(locationID)
+            .select("reviews")
+            .exec(function(err,location){
+                if(err){
+                    sendResponseJson(res,404,err);
+                } else {
+                    
+                }
+            })
+    } else{
+        sendResponseJson(res,404,{"message":"please check parameter"});
+    }
+}
+
+var doAddReview = function(req,res,location){
+    if(location){
+        location.reviews.push({
+            author:req.body.author,
+            rating:req.body.rating,
+            reviewText:req.body.reviewText
+        });
+        location.save(function(err,location){
+            var thisReview;
+            if(err){
+                sendResponseJson(res,404,err);
+            }else{
+                thisReview = location.reviews[location.reviews.length - 1];
+                updateAverageRating(location._id);
+                sendResponseJson(res,201,thisReview);
+            }
+        });
+    } else {
+        sendResponseJson(res,404,{"message":"Not find data from "+ req.params.locationid})
+    }
+};
+
+
+var updateAverageRating = function(locationID){
+    Loc.findById(locationID)
+        .select("rating reviews")
+        .exec(function(err,location){
+            if(err){
+                sendResponseJson(res,404,err);
+            } else {
+                doAverageRating(location);
+            }
+        });
+};
+
+var doAverageRating = function(location){
+    var i,reviewCount,ratingAverage, ratingTotal;
+    if(location.reviews && location.reviews.length > 0){
+        reviewCount = location.reviews.length;
+        ratingTotal = 0;
+        for(i=0;i<reviewCount;i++){
+            ratingTotal = ratingTotal +  location.reviews[i].rating;
+        }
+        ratingAverage = parseInt(ratingTotal/reviewCount,10);
+        location.rating = ratingAverage;
+        location.save(function(err){
+            if(err){
+                console.log(err);
+            } else{
+                console.log("Average rating updated to",ratingAverage);
+            }
+        });
+    };
+};
+
 module.exports.reviewsReadOne = function(req,res){
     if(req.params && req.params.locationid && req.params.reviewid){
         Loc.findById(req.params.locationid)
